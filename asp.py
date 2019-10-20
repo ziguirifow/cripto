b385b5cbd61e4d4ff52597b5a82fe1064973714f
ziguirifow <thulio.alves@gmail.com>
2019-10-20 18:37:12 -0300

    Update asp.py

--- asp.py
+++ asp.py
@@ -1,17 +1,41 @@
-import base64
-def encode(key, clear):
-    enc = []
-    for i in range(len(clear)):
-        key_c = key[i % len(key)]
-        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
-        enc.append(enc_c)
-    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+def encrypt(text,s): 
+        result = "" 
 
-def decode(key, enc):
-    dec = []
-    enc = base64.urlsafe_b64decode(enc).decode()
-    for i in range(len(enc)):
-        key_c = key[i % len(key)]
-        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
-        dec.append(dec_c)
-    return "".join(dec)
+        # traverse text 
+        for i in range(len(text)): 
+            char = text[i] 
+
+            # Encrypt uppercase characters 
+            if (char.isupper()): 
+                result += chr((ord(char) + s-65) % 26 + 65) 
+
+            # Encrypt lowercase characters 
+            else: 
+                result += chr((ord(char) + s - 97) % 26 + 97) 
+
+        return result 
+
+    def decrypt(text,s): 
+        result = "" 
+
+        # traverse text 
+        for i in range(len(text)): 
+            char = text[i] 
+
+            # Encrypt uppercase characters 
+            if (char.isupper()): 
+                result += chr((ord(char) - s-65) % 26 + 65) 
+
+            # Encrypt lowercase characters 
+            else: 
+                result += chr((ord(char) - s - 97) % 26 + 97) 
+
+        return result 
+
+    #check the above function 
+    text = "ATTACKATONCE"
+    s = 4
+    print("Text  : " + text) 
+    print("Shift : " + str(s)) 
+    print("Cipher: " + encrypt(text,s))
+    print("Original text: " + decrypt(encrypt(text,s),s))
