0617f0e94d7b7de1c33ae6b0d7e16c91c6d7b1f2
ziguirifow <thulio.alves@gmail.com>
2019-10-20 18:27:48 -0300

    Update asp.py

--- asp.py
+++ asp.py
@@ -1,32 +1,17 @@
-import Crypto
-from Crypto.PublicKey import RSA
-from Crypto import Random
-import ast
+import base64
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
-random_generator = Random.new().read
-key = RSA.generate(1024, random_generator) #generate pub and priv key
-
-publickey = key.publickey() # pub key export for exchange
-
-encrypted = publickey.encrypt('encrypt this message', 32)
-#message to encrypt is in the above line 'encrypt this message'
-
-print 'encrypted message:', encrypted #ciphertext
-f = open ('encryption.txt', 'w')
-f.write(str(encrypted)) #write ciphertext to file
-f.close()
-
-#decrypted code below
-
-f = open('encryption.txt', 'r')
-message = f.read()
-
-
-decrypted = key.decrypt(ast.literal_eval(str(encrypted)))
-
-print 'decrypted', decrypted
-
-f = open ('encryption.txt', 'w')
-f.write(str(message))
-f.write(str(decrypted))
-f.close()
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
